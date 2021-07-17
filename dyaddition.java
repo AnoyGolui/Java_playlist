02ea27df8b86ce479288f51706965e710679df41
AnoyGolui <67983302+AnoyGolui@users.noreply.github.com>
2021-07-18 02:07:55 +0530

    Create dyaddition.java

---
+++ dyaddition.java
@@ -0,0 +1,17 @@
+//Program to Accept values of Two Numbers and print their Addition.
+
+import java.util.Scanner;
+
+class AddNumbers
+{
+    public static void main(String args[])
+    {
+        int x, y, z;
+        System.out.print("Enter two integers to calculate their sum : ");
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        y = in.nextInt();
+        z = x + y;
+        System.out.println("Sum of entered integers = " + z);
+    }
+}
